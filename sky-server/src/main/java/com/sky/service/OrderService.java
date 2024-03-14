8410b7d61391d8988fafa13a3e47a4901115b8b4
wkz <1162580026@qq.com>
2024-03-14 16:13:47 +0800

    用户取消订单（退款，更新订单状态），再来一单（订单重新copy到购物车表）

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -35,5 +35,15 @@ public interface OrderService {
      * @return
      */
     OrderVO details(Long id);
-
+    /**
+     * 用户取消订单
+     * @param id
+     */
+    void userCancelById(Long id) throws Exception;
+    /**
+     * 再来一单
+     *
+     * @param id
+     */
+    void repetition(Long id);
 }
